fdf18fefa8f61ea2111bff1d1c9264a8994a1f8f
Jadenir <jadenir.p.s@gmail.com>
2020-06-07 15:17:53 -0300

    Anotations de validação pelo Razor tanto pelo client  quanto pelo server. Configuração dos scritps de jquery no _Layout.cshtml

--- TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
+++ TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,19 @@ namespace TreinaWeb.TesteASPNETMVC.Web.Models
 {
     public class Pessoa
     {
+        [Required(ErrorMessage = "O nome é obrigatório")]
         [DisplayName("Nome da pessoa")]
         public string Nome { get; set; }
+
         [DisplayName("Idade da pessoa")]
         public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório")]
         [DisplayName("Endereço da pessoa")]
         public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
+        [DisplayName("E-mail da pessoa")]
+        public string Email { get; set; }
     }
 }
