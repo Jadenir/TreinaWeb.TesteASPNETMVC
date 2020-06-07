8f3f7a03e17e2663ecce8be8610befc0f8012356
Jadenir <jadenir.p.s@gmail.com>
2020-06-07 14:56:12 -0300

    Data anotation do datamodel

--- TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
+++ TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace TreinaWeb.TesteASPNETMVC.Web.Models
 {
     public class Pessoa
     {
+        [DisplayName("Nome da pessoa")]
         public string Nome { get; set; }
+        [DisplayName("Idade da pessoa")]
         public int Idade { get; set; }
+        [DisplayName("Endereço da pessoa")]
         public string Endereco { get; set; }
     }
 }
