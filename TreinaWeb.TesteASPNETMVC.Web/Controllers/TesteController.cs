f8aaf58342af600a460b4fac2ae69f12779dd57c
Jadenir <jadenir.p.s@gmail.com>
2020-06-07 00:05:55 -0300

    Add project files.

---
+++ TreinaWeb.TesteASPNETMVC.Web/Controllers/TesteController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TreinaWeb.TesteASPNETMVC.Web.Models;
+
+namespace TreinaWeb.TesteASPNETMVC.Web.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            ViewBag.PessoaInformada = pessoa.Nome;
+            return View("Saudacao", pessoa);
+        }
+    }
+}
---
+++ TreinaWeb.TesteASPNETMVC.Web/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreinaWeb.TesteASPNETMVC.Web.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Endereco { get; set; }
+    }
+}
